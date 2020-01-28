87af5f20984b54a83ad1ae84207f4ad9514f9e8a
Edwin <edkerforne@users.noreply.github.com>
2020-01-28 15:32:21 +0100

    Updates

---
+++ precache-manifest.212e260d86c60b21a52986a0396eb627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ffb3dec103a6b27659048cd60819ab",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "7367daca365c4c108165",
+    "url": "/random-quote-machine/static/css/main.bcd0bef7.chunk.css"
+  },
+  {
+    "revision": "0cdfcf98da4bc3bf962e",
+    "url": "/random-quote-machine/static/js/2.d903d49c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/random-quote-machine/static/js/2.d903d49c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7367daca365c4c108165",
+    "url": "/random-quote-machine/static/js/main.6a2ad9d9.chunk.js"
+  },
+  {
+    "revision": "afc9b93c50eb3e83906a",
+    "url": "/random-quote-machine/static/js/runtime-main.daced29d.js"
+  }
+]);
